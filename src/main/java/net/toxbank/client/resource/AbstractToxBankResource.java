c81e351e27972747f62369c56ba78aa1f9199542
ngn <jeliazkova.nina@gmail.com>
2011-11-24 23:01:10 +0200

    added Serializable interface

--- src/main/java/net/toxbank/client/resource/AbstractToxBankResource.java
+++ src/main/java/net/toxbank/client/resource/AbstractToxBankResource.java
@@ -1,8 +1,9 @@
 package net.toxbank.client.resource;
 
+import java.io.Serializable;
 import java.net.URL;
 
-public abstract class AbstractToxBankResource implements IToxBankResource {
+public abstract class AbstractToxBankResource implements IToxBankResource , Serializable{
 
 	private URL resourceURL;
 
