41b465ea79918c01f0d1abf34fe7453acfc6a042
ngn <jeliazkova.nina@gmail.com>
2011-12-13 18:02:59 +0200

    added some maven profile tricks to switch the test server via profiles.

--- src/test/java/net/toxbank/client/resource/AbstractClientTest.java
+++ src/test/java/net/toxbank/client/resource/AbstractClientTest.java
@@ -1,17 +1,32 @@
 package net.toxbank.client.resource;
 
+import java.util.Properties;
+
 import org.junit.AfterClass;
 import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
 public abstract class AbstractClientTest<T extends IToxBankResource, C extends AbstractClient<T>> {
-	//public final static String TEST_SERVER = "http://localhost:8080/toxbank";
-	public final static String TEST_SERVER = "http://toxbanktest1.opentox.org:8080/toxbank";
-
+	//public final static String TEST_SERVER = "http://toxbanktest1.opentox.org:8080/toxbank";
+	public final static String TEST_SERVER = config();
+	//should be configured in the .m2/settings.xml 
+	protected static final String test_server_property = "toxbank.test.server";
 	@BeforeClass
 	public static void setup() {
-		
+		System.out.println(TEST_SERVER);
+	}
+	
+	public static String config()  {
+		String local = "http://localhost:8080/toxbank";
+		try {
+			Properties p = new Properties();
+			p.load(AbstractClientTest.class.getClassLoader().getResourceAsStream("net/toxbank/client/test/client.properties"));
+			String testServer = p.getProperty(test_server_property);
+			return testServer!=null?testServer.startsWith("http")?testServer:local:local;
+		} catch (Exception x) {
+			return local;
+		}
 	}
 	
 	@AfterClass
