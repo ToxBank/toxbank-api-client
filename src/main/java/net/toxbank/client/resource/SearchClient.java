ce499c9d0b229e8ab2b4efae58338672df89ec0c
ngn <jeliazkova.nina@gmail.com>
2012-05-24 07:37:56 +0300

    Added SearchClient class, configuration and test.

---
+++ src/main/java/net/toxbank/client/resource/SearchClient.java
@@ -0,0 +1,50 @@
+package net.toxbank.client.resource;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.List;
+
+import net.toxbank.client.exceptions.InvalidInputException;
+import net.toxbank.client.io.rdf.IOClass;
+import net.toxbank.client.io.rdf.ProtocolIO;
+import net.toxbank.client.policy.PolicyRule;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.client.HttpClient;
+import org.opentox.rest.RestException;
+
+public class SearchClient extends AbstractClient<Protocol> {
+	private static final String metadatasearch_param = "metadata";
+	public SearchClient() {
+		this(null);
+	}
+		
+	public SearchClient(HttpClient httpclient) {
+		super(httpclient);
+	}
+	
+	@Override
+	IOClass<Protocol> getIOClass() {
+		return new ProtocolIO();
+	}
+
+	
+	@Override
+	public List<URL> searchURI(URL url,String query) throws  RestException, IOException {
+		return listURI(url, new String[] {metadatasearch_param,query});
+	}
+	
+	@Override
+	protected HttpEntity createPOSTEntity(Protocol object,
+			List<PolicyRule> accessRights) throws InvalidInputException,
+			Exception {
+		throw new Exception("Not implemented");
+	}
+
+	@Override
+	protected HttpEntity createPUTEntity(Protocol object,
+			List<PolicyRule> accessRights) throws InvalidInputException,
+			Exception {
+		throw new Exception("Not implemented");
+	}
+}
