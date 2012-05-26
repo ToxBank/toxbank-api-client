923a3a6d675d3f92bcdc20939ca41e2c984eefb7
ngn <jeliazkova.nina@gmail.com>
2012-05-26 11:34:01 +0300

    site

--- src/main/java/net/toxbank/client/resource/AlertClient.java
+++
@@ -1,7 +0,0 @@
-package net.toxbank.client.resource;
-
-public class AlertClient {
-
-	private AlertClient() {}
-
-}
--- src/main/java/net/toxbank/client/resource/DocumentClient.java
+++
@@ -1,7 +0,0 @@
-package net.toxbank.client.resource;
-
-public class DocumentClient {
-
-	private DocumentClient() {}
-
-}
