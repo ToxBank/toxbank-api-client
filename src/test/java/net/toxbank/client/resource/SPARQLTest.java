e13ca71ba30152a7a5d423b77fc9f3799b71d1c3
ngn <jeliazkova.nina@gmail.com>
2014-03-14 15:16:27 +0200

    Improved query for files by investigation

--- src/test/java/net/toxbank/client/resource/SPARQLTest.java
+++ src/test/java/net/toxbank/client/resource/SPARQLTest.java
@@ -39,7 +39,7 @@ public class SPARQLTest {
 				 return "https://services.toxbank.net/investigation/3e78e694-4656-42c5-a26b-77b00c306633/I67";
 			}
 		},
-		dium {
+		dimu {
 			@Override
 			public String getInvestigationURI() {
 				return "https://services.toxbank.net/investigation/d230d110-9630-4263-bdaf-0936dcdf582f/I1";
@@ -55,7 +55,7 @@ public class SPARQLTest {
 	public void test_endpoint_and_technology_by_investigation() throws Exception {
 		Assert.assertNotNull(model);
 		String sparqlQuery = String.format(
-				loadQuery("endpoint_and_technology_by_investigation","{investigation_uri}"),
+				loadQuery("endpoint_and_technology_by_investigation",new String[]{"{investigation_uri}"}),
 				demo.getInvestigationURI());
 		Assert.assertTrue(execQuery(sparqlQuery)>0);
 
@@ -66,17 +66,31 @@ public class SPARQLTest {
 		Assert.assertNotNull(model);
 		if (demo.equals(_test.tggates)) return ; //no files there?
 		String sparqlQuery = String.format(
-				loadQuery("files_by_investigation","{investigation_uri}"),
+				loadQuery("files_by_investigation",new String[]{"{investigation_uri}"}),
 				demo.getInvestigationURI());
 		Assert.assertTrue(execQuery(sparqlQuery)>0);
 
 	}
+
+	
+	@Test
+	public void test_files_and_assays_by_investigation() throws Exception {
+		Assert.assertNotNull(model);
+		if (demo.equals(_test.tggates)) return ; //no files there?
+		String sparqlQuery = String.format(
+				loadQuery("files_and_assays_by_investigation",new String[]{"{investigation_uri}"}),
+				demo.getInvestigationURI(),demo.getInvestigationURI(),demo.getInvestigationURI());
+		Assert.assertTrue(execQuery(sparqlQuery)>0);
+
+	}
+
+	
 	
 	@Test
 	public void test_characteristics_by_investigation() throws Exception {
 		Assert.assertNotNull(model);
 		String sparqlQuery = String.format(
-				loadQuery("characteristics_by_investigation","{investigation_uri}"),
+				loadQuery("characteristics_by_investigation",new String[]{"{investigation_uri}"}),
 				demo.getInvestigationURI());
 		Assert.assertTrue(execQuery(sparqlQuery)>0);
 	}
@@ -85,12 +99,12 @@ public class SPARQLTest {
 	public void test_characteristics_by_sample() throws Exception {
 		Assert.assertNotNull(model);
 		String sparqlQuery = String.format(
-				loadQuery("characteristics_by_sample","{sample_uri}"),
+				loadQuery("characteristics_by_sample",new String[]{"{sample_uri}"}),
 				"https://services.toxbank.net/investigation/6c81b6f9-1684-41e6-ae02-e1b45ef60741/source417");
 		Assert.assertTrue(execQuery(sparqlQuery)>0);
 		
 		sparqlQuery = String.format(
-				loadQuery("characteristics_by_sample","{sample_uri}"),
+				loadQuery("characteristics_by_sample",new String[]{"{sample_uri}"}),
 				"https://services.toxbank.net/investigation/6c81b6f9-1684-41e6-ae02-e1b45ef60741/source405");
 
 	
@@ -103,7 +117,7 @@ public class SPARQLTest {
 		Assert.assertNotNull(model);
 		String sparqlQuery = null;
 			sparqlQuery = String.format(
-				loadQuery("factorvalues_by_investigation","{investigation_uri}"),
+				loadQuery("factorvalues_by_investigation",new String[]{"{investigation_uri}"}),
 				demo.getInvestigationURI());
 
 		Assert.assertTrue(
@@ -112,7 +126,7 @@ public class SPARQLTest {
 						String biosample = qs.getResource("biosample").getURI();
 						try {
 							String subSelect = String.format(
-									loadQuery("characteristics_by_sample","{sample_uri}"),
+									loadQuery("characteristics_by_sample",new String[]{"{sample_uri}"}),
 									biosample);
 							Assert.assertTrue(execQuery(subSelect)>0);
 						} catch (Exception x) {
@@ -137,15 +151,20 @@ public class SPARQLTest {
 		} finally {
 			qe.close();	
 		}
+		System.err.println(records);
 		return records;
 	}
-	public String loadQuery(String sparqlQuery,String param) throws Exception {
+	public String loadQuery(String sparqlQuery,String[] params) throws Exception {
 		String q = String.format("net/toxbank/client/sparql/%s.sparql", sparqlQuery);
 		InputStream in = TBClient.class.getClassLoader().getResourceAsStream(q);
 		Assert.assertNotNull(in);
 		try {
 	        //Z means: "The end of the input but for the final terminator, if any"
-	     return new java.util.Scanner(in,"UTF8").useDelimiter("\\Z").next().replace(param,"s");
+			String query = new java.util.Scanner(in,"UTF8").useDelimiter("\\Z").next();
+			for (String param : params) 
+				query = query.replace(param,"s");
+			
+			return query;
 		} finally {
 			if (in!=null) in.close();
 		}
@@ -179,6 +198,7 @@ public class SPARQLTest {
 class ProcessSolution {
 	public int process(ResultSet rs) {
 		int records = 0;
+		processHeader(rs);
 		System.out.println();
 		while (rs.hasNext()) {
 			records++;
